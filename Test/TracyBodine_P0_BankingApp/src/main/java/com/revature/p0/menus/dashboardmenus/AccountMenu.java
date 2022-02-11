f6bc4e656ca5fafd220c72abd51014ff2de33997
tbodine <39622946+tbodine@users.noreply.github.com>
2022-02-10 22:30:40 -0600

    commit after fixing account menu

--- Test/TracyBodine_P0_BankingApp/src/main/java/com/revature/p0/menus/dashboardmenus/AccountMenu.java
+++ Test/TracyBodine_P0_BankingApp/src/main/java/com/revature/p0/menus/dashboardmenus/AccountMenu.java
@@ -8,15 +8,15 @@ import com.revature.p0.services.CustomerService;
 import com.revature.p0.util.MenuRouter;
 
 public class AccountMenu extends Menu {
-	
+
 	private final CustomerService customerService;
 	private Customer sessionCustomer;
-	
+
 	public AccountMenu(BufferedReader consoleReader, MenuRouter router, CustomerService customerService) {
 		super("Account", "/account", consoleReader, router);
 		this.customerService = customerService;
 	}
-	
+
 	@Override
 	public void render() throws Exception {
 
@@ -27,47 +27,38 @@ public class AccountMenu extends Menu {
 			router.transfer("/login");
 			return;
 		}
-		
-			while (CustomerService.isSessionActive()) {
-				System.out.println("\nWelcome " + sessionCustomer.getUsername() + "\n\nMain Menu" + "\n\nPlease select from the following options");
-				String menu = 
-						"1) View my Accounts\n" + 
-						"2) Make a depost\n" +
-						"3) Make a withdrawl\n" + 
-						"4) Return to main menu\n" + 
-						 "> ";
 
-				System.out.print(menu);
+		while (customerService.isSessionActive()) {
+			System.out.println("\nWelcome " + sessionCustomer.getUsername() + "\n\nMain Menu"
+					+ "\n\nPlease select from the following options");
+			String menu = "1) View my Accounts\n" + "2) Make a depost\n" + "3) Make a withdrawl\n"
+					+ "4) Return to main menu\n" + "> ";
 
-				String userSelection = consoleReader.readLine();
+			System.out.print(menu);
 
-				switch (userSelection) {
-				case "1":
-					System.out.println("View my Accounts Selected");
-					router.transfer("/my-accounts");
-					break;
-				case "2":
-					System.out.println("Make a deposit selected");
-					router.transfer("/deposit");
-					break;
-				case "3":
-					System.out.println("Make a withdrawl selected:");
-					router.transfer("/withdrawl");
-					break;
-				case "4":
-					System.out.println("Return to main menu:");
-					router.transfer("/dashboard");
-					break;
-				default:
-					System.out.println("Invalid selection. Pleas try again.");
-				}
+			String userSelection = consoleReader.readLine();
+
+			switch (userSelection) {
+			case "1":
+				System.out.println("View my Accounts Selected");
+				router.transfer("/my-accounts");
+				break;
+			case "2":
+				System.out.println("Make a deposit selected");
+				router.transfer("/deposit");
+				break;
+			case "3":
+				System.out.println("Make a withdrawl selected:");
+				router.transfer("/withdrawl");
+				break;
+			case "4":
+				System.out.println("Return to main menu:");
+				router.transfer("/dashboard");
+				break;
+			default:
+				System.out.println("Invalid selection. Pleas try again.");
 			}
 		}
-
-	@Override
-	public void render() throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
-	}
+}
--- Test/TracyBodine_P0_BankingApp/src/main/java/com/revature/p0/menus/dashboardmenus/DepositMenu.java
+++ Test/TracyBodine_P0_BankingApp/src/main/java/com/revature/p0/menus/dashboardmenus/DepositMenu.java
@@ -2,28 +2,60 @@ package com.revature.p0.menus.dashboardmenus;
 
 import java.io.BufferedReader;
 
+import com.revature.p0.exceptions.InvalidRequestException;
 import com.revature.p0.menus.Menu;
 import com.revature.p0.models.Account;
+import com.revature.p0.models.Customer;
 import com.revature.p0.services.AccountService;
+import com.revature.p0.services.CustomerService;
 import com.revature.p0.util.MenuRouter;
 
-public class DepositMenu {
+public class DepositMenu extends Menu {
 
-	public DepositMenu(BufferedReader consoleReader, MenuRouter router) {
+	private AccountService accountService;
+
+		public DepositMenu(BufferedReader consoleReader, MenuRouter router, AccountService accountService) {
 		super("Deposit", "/deposit", consoleReader, router);
-
+		this.accountService = accountService;
 	}
 
 	
-	//need to figure out how to display accounts from database
 	@Override
 	public void render() throws Exception {
 		
-		System.out.println("Account view selected");
+				System.out.println("\nNew Deposit\n");
 
-		//Need to pull data from database
-		System.out.println("Here is a list of your accounts.");
+				// Information needed from the user: first name, last name, email, username, password
+				System.out.println("Please provid deposit details");
+				System.out.print("1 - Account Name: ");
+				String accountName = consoleReader.readLine();
 
+				System.out.print("2 - Deposit Amount: ");
+				String depositAmount = consoleReader.readLine();
+				
 	}
-
 }
+
+//				Customer customer = new Customer(accountName, depositAmount);
+//
+//				try {
+//					customerService.registerNewCustomer(customer);
+//				} catch (InvalidRequestException e) {
+//
+//					System.out.println("THE INFORMATION YOU ENTERED IS INVALID, PLEASE TRY AGAIN\n\n\n");
+//
+//					router.transfer("/welcome");
+//				}
+	
+	
+//	//need to figure out how to display accounts from database
+//	@Override
+//	public void render() throws Exception {
+//		
+//		System.out.println("Account view selected");
+//
+//		//Need to pull data from database
+//		System.out.println("Here is a list of your accounts.");
+//
+//	}
+
